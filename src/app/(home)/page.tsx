ff3b8661ff897200f856a40e7174b3eda7f6e229
ibrahim kedir <techofreact@gmail.com>
2025-03-23 18:00:21 +0300

    refactor: show relevant information only.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,8 +26,7 @@ export default async function Home({ searchParams }: PropsType) {
       <Suspense fallback={<OverviewCardsSkeleton />}>
         <OverviewCardsGroup />
       </Suspense>
-      <h1>Hello</h1>
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+      {/* <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
         <PaymentsOverview
           className="col-span-12 xl:col-span-7"
           key={extractTimeFrame("payments_overview")}
@@ -57,7 +56,7 @@ export default async function Home({ searchParams }: PropsType) {
         <Suspense fallback={null}>
           <ChatsCard />
         </Suspense>
-      </div>
+      </div> */}
     </LayoutContainer>
   );
 }
